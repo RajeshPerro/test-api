f8f5e57b12d4502ec61031a2c2cb759c7946eb28
Rajesh Ghosh <rajeshghosh4007@gmail.com>
2022-01-23 02:25:04 +0100

    user endpoint test udpated

--- tests/ApiCest.php
+++ tests/ApiCest.php
@@ -18,4 +18,22 @@ class ApiCest
             'modified_at' => 'string',
         ]);
     }
+
+    public function tryGetOneUserApi(ApiTester $I)
+    {
+        $I->sendGet('/user/get/1');
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([
+            "id"=> "1",
+            "user_name" => "rajesh10",
+            "user_email" => "rajesh_test@myapp.com",
+            "password" => "test_pass_23",
+            "mobile_number" => "+44 78267262",
+            "address" => "address_test",
+            "created_at" => "2022-01-22 21:08:28",
+            "modified_at"=> "2022-01-22 21:46:56"
+        ]);
+    }
 }
